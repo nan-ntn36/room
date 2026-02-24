335b8d0ed1353824ad2b39f4741477a96460d3ea
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2026-02-25 01:00:06 +0700

    feat: add preview in home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import type { Route } from './+types/home';
 import Navbar from '../../components/Navbar';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, ArrowUpRight, Clock, Layers } from 'lucide-react';
 import { Button } from 'components/ui/Button';
+import preview from '../../src/asset/img/rendered.png';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -33,6 +34,52 @@ export default function Home() {
           <Button size="lg" variant="outline">
             Get Started
           </Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, and PDF formats, up to 100MB in size.</p>
+            </div>
+            <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared projects, all in one place.</p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src={preview} alt="Project preview" />
+                <div className="badge">
+                  <span>Cummunity</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Modern Villa</h3>
+                  <div className="meta">
+                    <Clock className="icon" />
+                    <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                    <span>Yoshi</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight className="icon" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
